74e420beaa5380d7e94a223b3ad6ee102ec961b4
Del <aetherswap15@gmail.com>
2025-11-30 17:49:00 +0700

    fix hardhat import

--- tasks/deploy/shannon.ts
+++ tasks/deploy/shannon.ts
@@ -1,9 +1,10 @@
 import { task, types } from "hardhat/config";
 import { ethers } from "hardhat";
+import type { HardhatRuntimeEnvironment } from "hardhat/types";
 
 import shannonConfig from "./constants/shannonConfig";
 
-async function getInitCode(hre: any, name: string, args: any[]) {
+async function getInitCode(hre: HardhatRuntimeEnvironment, name: string, args: any[]) {
   const f = await hre.ethers.getContractFactory(name);
   return f.getDeployTransaction(...args).data!;
 }
@@ -13,7 +14,7 @@ async function getInitCodeHash(initCode: string) {
 }
 
 task("deploy:create2factory", "Deploy CREATE2Factory contract")
-  .setAction(async function (_, hre) {
+  .setAction(async function (_, hre: HardhatRuntimeEnvironment) {
     const [deployer] = await hre.ethers.getSigners();
     console.log("Deploying CREATE2Factory with Address:", deployer.address);
 
@@ -24,11 +25,11 @@ task("deploy:create2factory", "Deploy CREATE2Factory contract")
   });
 
 task("deploy:shannon", "Deploys Oka with Shannon config")
-  .addParam("initializeMinter", "Whether to run minter.initialize", "false", types.boolean)
-  .addParam("useCreate2", "Deploy Oka with CREATE2", "false", types.boolean)
+  .addParam("initializeMinter", "Whether to run minter.initialize", false, types.boolean)
+  .addParam("useCreate2", "Deploy Oka with CREATE2", false, types.boolean)
   .addParam("salt", "Salt for CREATE2 deployment", "Okaswap", types.string)
   .addParam("factory", "CREATE2Factory address used for vanity deployment", "0x0000000000000000000000000000000000000000", types.string)
-  .setAction(async function (taskArguments, hre) {
+  .setAction(async function (taskArguments, hre: HardhatRuntimeEnvironment) {
 
     const ACONFIG = shannonConfig;
     const gasPrice = hre.ethers.utils.parseUnits(ACONFIG.PRICE, 'wei');
@@ -167,9 +168,10 @@ task("deploy:shannon", "Deploys Oka with Shannon config")
     console.log("MerkleClaim deployed:", claim.address);
 
     // -----------------------------
-    // INITIALIZE (tetap sama)
+    // INITIALIZE
     // -----------------------------
-    await (await ethers.getContractAt("Oka", okaAddress)).initialMint(ACONFIG.teamEOA, { gasPrice });
+    const okaContract = await hre.ethers.getContractAt("Oka", okaAddress);
+    await okaContract.initialMint(ACONFIG.teamEOA, { gasPrice });
     console.log("Initial minted");
 
     console.log("Successfully deployed Oka to Shannon (⁠ﾉ⁠◕⁠ヮ⁠◕⁠)⁠ﾉ⁠*⁠.⁠✧");
